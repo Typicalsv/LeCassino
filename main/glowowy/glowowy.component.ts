738807a6b02d153eb716a5880426e4e9a93325b6
Kermit-bez-bana <kacperkolator646@gmail.com>
2025-03-26 13:24:29 +0100

    Add files via upload

---
+++ main/glowowy/glowowy.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-glowowy',
+  imports: [],
+  templateUrl: './glowowy.component.html',
+  styleUrl: './glowowy.component.css'
+})
+export class GlowowyComponent {
+  title = 'lecassino';
+  account_name = 'Degen';
+  account_balance = '1000BTC';
+
+}
