ef5284e6741b409094a0dcc63c0bbcc58eaefba0
Kermit-bez-bana <kacperkolator646@gmail.com>
2025-04-01 02:04:03 +0200

    Update dices.component.ts

--- games/Dice_Game/dices.component.ts
+++ games/Dice_Game/dices.component.ts
@@ -1,7 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
 import { AppComponent } from '../app.component';
 @Component({
   selector: 'app-dices',
