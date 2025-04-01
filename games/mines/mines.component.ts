681e9bf1fa17abf29478e98e965ee988733a93e9
Kermit-bez-bana <kacperkolator646@gmail.com>
2025-04-01 02:06:14 +0200

    Update mines.component.ts

--- games/mines/mines.component.ts
+++ games/mines/mines.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { GlowowyComponent } from '../glowowy/glowowy.component';
 import { AppComponent } from '../app.component';
 
 @Component({
